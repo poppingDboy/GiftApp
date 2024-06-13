a1cb5fe0e19df60c4cb70593c45dd3af760dad2a
Dylan Marteau <dylan_marteau@connect-tech.sncf>
2024-06-13 16:16:11 +0200

    Initial Commit

---
+++ Gift/GiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftApp.swift
+//  Gift
+//
+//  Created by MARTEAU Dylan on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
